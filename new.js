d1fe33f9889c25eb497f15365c2c1ba57e4184df
Andrews Mensah <andrews.mensah@amalitech.org>
2021-06-16 11:26:27 +0000

    first

---
+++ new.js
@@ -0,0 +1,29 @@
+let leapYearCalc = () => {
+    let year = document.getElementById('year').value;
+
+    console.log (year);
+
+
+    if (year >=1 && year <= 9999) {
+        if (year % 4 == 0 && year % 100 == 0 && year % 400 == 0) {
+            document.querySelector('p').innerHTML = "The year " + year + " is a leap year";
+
+            console.log ("The year" + year + "is a leap year");
+        }
+
+        else {
+            document.querySelector('p').innerHTML = "The year" + year + "is not a leap year";
+
+        }
+    }
+
+}
+
+
+const btn = document.getElementById ("btn");
+
+btn.addEventListener("mouseup", (e) => {leapYearCalc()
+
+    console.log(e);
+
+})
